3df0866c296b6041b60c1d77111976f3fe2a6556
Michael Heap <m@michaelheap.com>
2016-11-06 00:10:26 +0000

    Change message

--- web/index.php
+++ web/index.php
@@ -5,7 +5,7 @@ require_once __DIR__.'/../vendor/autoload.php';
 $app = new \Silex\Application;
 
 $app->get("/", function(){
-    return 'Hello World';
+    return 'This should auto-deploy';
 });
 
 $app->run();
