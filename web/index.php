4252e6a3307cd3776cd8ff41a42a081680483110
Michael Heap <m@michaelheap.com>
2016-11-06 10:14:58 +0000

    Change to trigger a new deploy for DB migrations

--- web/index.php
+++ web/index.php
@@ -5,7 +5,7 @@ require_once __DIR__.'/../vendor/autoload.php';
 $app = new \Silex\Application;
 
 $app->get("/", function(){
-    return 'Hello World';
+    return 'Database please';
 });
 
 $app->run();
